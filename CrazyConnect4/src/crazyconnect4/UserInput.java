1c90945f6bc1f9ed986585913b7b82909ab743d5
Henry Diaz <henrydiazlds123@hotmail.com>
2014-05-30 15:05:51 -0400

    Merge origin/master
    Conflicts:
    	CrazyConnect4/src/crazyconnect4/UserInput.java

--- CrazyConnect4/src/crazyconnect4/UserInput.java
+++ CrazyConnect4/src/crazyconnect4/UserInput.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
-  */
+ */
 package crazyconnect4;
 
 import java.io.IOException;
@@ -14,33 +14,35 @@ import java.util.Scanner;
  */
 public class UserInput {
 
-  String usInput;
-  int count = 1;
+    String usInput;
+    int count = 1;
 
-  public void userInput() {
-  }
+    public void userInput() {
+    }
 
-  public void playerInput() {
-  Scanner input = new Scanner(System.in);
-  System.out.println("Enter a value between 1 and 10: ");
-  this.usInput = input.nextLine();
-  int userIn = Integer.parseInt(usInput);
+    public void playerInput() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a value between 1 and 10: ");
+        this.usInput = input.nextLine();
+        int userIn = Integer.parseInt(usInput);
 
-  while (userIn >=1 && userIn <= 10) {
-  System.out.println("This is a counter: " + userIn);
-  count +=2;
-  userIn += 1;
-  if (userIn == 10) continue;
-   
-  }
-  if (userIn > 10 && count == 1) {
-  System.out.println("The value entered is incorrect.");
-  }
+        while (userIn >= 1 && userIn <= 10) {
+            System.out.println("This is a counter: " + userIn);
+            count += 2;
+            userIn += 1;
+            if (userIn == 10) {
+                continue;
+            }
 
-  }
+        }
+        if (userIn > 10 && count == 1) {
+            System.out.println("The value entered is incorrect.");
+        }
 
-  public static void main(String[] args) {
-  UserInput userinput = new UserInput();
-  userinput.playerInput();
-  }
+    }
+
+    public static void main(String[] args) {
+        UserInput userinput = new UserInput();
+        userinput.playerInput();
+    }
 }
