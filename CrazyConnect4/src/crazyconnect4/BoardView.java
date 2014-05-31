c61c2cfcc6f4e4c3ba37d788785c48ff906479b2
kylugo <kylugo@192.168.2.3>
2014-05-31 14:59:33 -0600

    Merge origin/master
    Conflicts:
    	CrazyConnect4/src/crazyconnect4/UserInput.java

---
+++ CrazyConnect4/src/crazyconnect4/BoardView.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package crazyconnect4;
+
+/**
+ *
+ * @author kylugo
+ */
+public class BoardView 
+{
+    void viewBoard()  
+    {
+        int i;
+        int j;
+
+        for (i = 0; i < 10; i++) 
+        {
+            for (j = 0; j < 10; j++) 
+                System.out.print(i);
+            System.out.print(j);
+        }
+    }
+    
+}
