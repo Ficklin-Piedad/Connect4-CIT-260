172ebf72a58ee4f86f526428d4d83a7bd8c0ac6e
kylugo <kylugo@192.168.2.3>
2014-06-26 21:25:59 -0600

    Merge origin/master
    Conflicts:
    	CrazyConnect4/src/crazyconnect4/Statistics.java
    	CrazyConnect4/src/crazyconnect4/StatisticsMenu.java

---
+++ CrazyConnect4/src/crazyconnect4/HelpMenu.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package crazyconnect4;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author kylugo
+ */
+class HelpMenu extends Menu {
+
+    static final String[][] helpMenuItems = {
+        {"I", "Display Instructions"},
+        {"K", "Display Keyboard Functions"},
+        {"S", "Statistics"},
+        {"Q", "Exit"},};
+
+    private final HelpMenuControl helpMenuControl = new HelpMenuControl();
+
+    @Override
+    public void getInput2() {
+        super.getInput2(); //To change body of generated methods, choose Tools | Templates.
+
+        do {
+
+            this.displayMenu2(); // display the menu
+
+            // get commaned entered
+            userInput = inFile.nextLine();
+            userInput = userInput.trim().toUpperCase();
+
+            switch (userInput) {
+                case "I":
+                    this.helpMenuControl.displayInstructions();
+                    break;
+                case "K":
+                    this.helpMenuControl.displayKeyboardFunctions();
+                    break;
+                case "S":
+                    this.helpMenuControl.displayStatistics();
+                    break;
+                case "Q":
+                    this.helpMenuControl.displayExit();
+                    break;
+                default:
+                    System.out.println("Invalid. Please enter a valid command.");
+
+            }
+        } while (!userInput.equals("Q"));
+    }
+        public static void main(String[] args) throws IOException, InterruptedException {
+        HelpMenu menu = new HelpMenu();
+        menu.getInput();
+        
+    }//end Help
+}
+
