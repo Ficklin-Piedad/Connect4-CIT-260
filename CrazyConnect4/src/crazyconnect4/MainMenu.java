b67a77eea55d88e29576b83cc6bf6295551885bf
Henry Diaz <henrydiazlds123@hotmail.com>
2014-06-26 22:42:55 -0400

    changes

---
+++ CrazyConnect4/src/crazyconnect4/MainMenu.java
@@ -0,0 +1,70 @@
+package crazyconnect4;
+
+import java.io.IOException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author henrydiazlds
+ */
+class MainMenu extends Menu {
+
+    static final String[][] mainMenuItems = {
+        {"P", "One Player"},
+        {"T", "Two Players"},
+        {"S", "Statistics"},
+        {"O", "Options"},
+        {"H", "Help"},
+        {"Q", "Exit"},};
+
+    private final MainMenuControl mainMenuControl = new MainMenuControl();
+
+    @Override
+    public void getInput() {
+        super.getInput(); //To change body of generated methods, choose Tools | Templates.
+
+        do {
+
+            this.displayMenu(); // display the menu
+
+            // get commaned entered
+            userInput = inFile.nextLine();
+            userInput = userInput.trim().toUpperCase();
+
+            switch (userInput) {
+                case "P":
+                    this.mainMenuControl.onePlayer();
+                    break;
+                case "T":
+                    this.mainMenuControl.twoPlayer();
+                    break;
+                case "S":
+                    this.mainMenuControl.displayStatistics();
+                    break;
+                case "O":
+                    this.mainMenuControl.displayOptions();
+                    break;
+                case "H":
+                    this.mainMenuControl.displayHelp();
+                    break;
+                case "Q":
+                    this.mainMenuControl.byeBye();
+                    break;
+                default:
+                    System.out.println("Invalid. Please enter a valid command.");
+
+            }
+        } while (!userInput.equals("Q"));
+    }
+        public static void main(String[] args) throws IOException, InterruptedException {
+        MainMenu menu = new MainMenu();
+        menu.getInput();
+        
+    }//end Main
+}
+
--- CrazyConnect4/src/crazyconnect4/Menu.java
+++ CrazyConnect4/src/crazyconnect4/Menu.java
@@ -1,18 +1,5 @@
 package crazyconnect4;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Piedad
- */
-
-   
-
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -22,30 +9,28 @@ import java.util.Scanner;
  */
 public class Menu {
 
-  String userInput;
-  Scanner inFile = new Scanner(System.in);
+    String userInput;
+    Scanner inFile = new Scanner(System.in);
 
-  Menu() {
+    Menu() {
 
-  }
+    }
 
-  public void getInput() {
-  this.displayMenu();
+    public void getInput() {
+        this.displayMenu();
 
-  // get commaned entered
-  userInput = inFile.nextLine();
-  userInput = userInput.trim().toUpperCase();
+        // get commaned entered
+        userInput = inFile.nextLine();
+        userInput = userInput.trim().toUpperCase();
 
-  }
+    }
 
-  // displays the help menu
-  public final void displayMenu() {
-  System.out.println("\tEnter the letter associated with one of the following commands:");
+    // displays the help menu
+    public final void displayMenu() {
+        System.out.println("\tEnter the letter associated with one of the following commands:");
 
-  for (String[] menuItem : MainMenu.mainMenuItems) {
-  System.out.println("\t " + menuItem[0] + "\t" + menuItem[1]);
-  }
-  }
+        for (String[] menuItem : MainMenu.mainMenuItems) {
+            System.out.println("\t   " + menuItem[0] + "\t" + menuItem[1]);
+        }
+    }
 }
- 
-
