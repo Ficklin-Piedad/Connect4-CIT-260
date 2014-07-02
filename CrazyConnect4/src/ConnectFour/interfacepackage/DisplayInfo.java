be80fc027399cacd9cad12b56311771e7c825e5a
lugoky <https://github.com/henrdiazlds123/CIT260.git>
2014-07-01 18:57:55 -0600

    Merge origin/master

---
+++ CrazyConnect4/src/ConnectFour/interfacepackage/DisplayInfo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this templaConnectFour.interfacepackagete file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ConnectFour.interfacepackage;
+
+/**
+ *
+ * @author kylugo
+ */
+public interface DisplayInfo {
+    
+   void display();
+   
+    //here goes something else
+   
+}
